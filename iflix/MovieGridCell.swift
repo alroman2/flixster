a69818bbcf2291c46110d4e6a7efde2eaaad1ad1
Alexander Roman <alex37888@gmail.com>
2020-10-30 19:54:49 -0700

    New Movie Grid Layout for super hero Genre

---
+++ iflix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  iflix
+//
+//  Created by Alexander Roman Montiel on 10/30/20.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
